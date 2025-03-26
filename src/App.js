a3cd317235aadf9bc1248a4a948b61ac72592a92
sachin274 <sachin25095@gmail.com>
2025-03-26 17:23:42 +0530

    v2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import ArticlesPage from './ArticlesPage';
+import CreatePost from './CreatPost';
 
 function App() {
   return (
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './HomePage.css';
 import './ArticlesPage.css'
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
+import CreatePost from './CreatPost';
+
 
 function HomePage() {
   
-  // const App = () => {
-  //   const [articles, setArticles] = useState([]);
+  const App = () => {
+    const [articles, setArticles] = useState([]);
   
-  //   const addArticle = (newArticle) => {
-  //     setArticles([...articles, newArticle]);
-  //   };  
-  // }
+    const addArticle = (newArticle) => {
+      setArticles([...articles, newArticle]);
+    };  
+  }
   
   const navigate = useNavigate();
   return (
